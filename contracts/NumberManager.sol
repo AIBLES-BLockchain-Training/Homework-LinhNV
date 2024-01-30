5900c952196473857e4651f3ed59b49f4e099b20
Linh Van Nguyen <linhvnguyen9@gmail.com>
2024-01-30 16:34:26 +0700

    Add NumberManager

---
+++ contracts/NumberManager.sol
@@ -0,0 +1,39 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.23;
+
+contract NumberManager {
+
+    uint private totalSum;
+    uint public lastAddedNumber;
+
+    constructor(uint initialTotalSum) {
+        totalSum = initialTotalSum;
+        lastAddedNumber = 0;
+    }
+
+    function addNumber(uint number) public {
+        incrementTotalSum(number);
+        lastAddedNumber = number;
+    }
+
+    function subtractNumber(uint number) public {
+        decrementTotalSum(number);
+        setLastAddedNumber(number);
+    }
+
+    function getTotalSum() external view returns(uint) {
+        return totalSum;
+    }
+
+    function incrementTotalSum(uint number) private {
+        totalSum += number;
+    }
+
+    function decrementTotalSum(uint number) private {
+        totalSum -= number;
+    }
+
+    function setLastAddedNumber(uint number) internal {
+        lastAddedNumber = number;
+    }
+}
